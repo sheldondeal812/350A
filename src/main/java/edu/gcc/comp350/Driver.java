3dd901672c8e84b26f1da920fd081fb6c130c7c2
sheldondeal812 <sheldondeal812@gmail.com>
2026-02-06 14:46:02 -0500

    added line to main

--- src/main/java/edu/gcc/comp350/Driver.java
+++ src/main/java/edu/gcc/comp350/Driver.java
@@ -2,8 +2,9 @@ package edu.gcc.comp350;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello jar");
+        System.out.println("Hello there");
         Calculator c = new Calculator();
         System.out.println(c.add(4,5));
+        System.out.println("Hello");
     }
 }
